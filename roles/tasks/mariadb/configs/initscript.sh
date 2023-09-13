fc803cd60c7ee37fe96cf95aef11642bf4991765
ytaousi <yasssir.taous@gmail.com>
2023-09-13 17:17:42 +0100

    ... ok

--- roles/tasks/mariadb/configs/initscript.sh
+++ roles/tasks/mariadb/configs/initscript.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 rc-service mysql restart
 mysql < mysql_secure_installation.sql -u root -proot
-mysql < create_users.sql -u root -prootpassword
+mysql < create_users.sql -u root -p$DB_ROOT_PASS
+mysql < config_wp.sql -u root -p$DB_ROOT_PASS
 rc-service mysql restart
