f93c988d03cc117545d60888438204606449ea46
ytaousi <yasssir.taous@gmail.com>
2023-09-13 20:11:32 +0100

    .. socket issues

--- roles/tasks/mariadb/configs/initscript.sh
+++ roles/tasks/mariadb/configs/initscript.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
-rc-service mysql restart
-mysql < mysql_secure_installation.sql -u root -proot
-mysql < create_users.sql -u root -p$DB_ROOT_PASS
-mysql < config_wp.sql -u root -p$DB_ROOT_PASS
-rc-service mysql restart
+# rc-service mysql start
+# mysql < mysql_secure_installation.sql 
+# mysql < create_users.sql -u root -p$DB_ROOT_PASS
+# mysql < config_wp.sql -u root -p$DB_ROOT_PASS
+# rc-service mysql restart
+
+sleep 3000
