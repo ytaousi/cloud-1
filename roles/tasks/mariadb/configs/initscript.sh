843ded4e7e133e29a65ee804393874a589e2311f
ytaousi <yasssir.taous@gmail.com>
2023-09-14 15:55:46 +0100

    ... db is crashing

--- roles/tasks/mariadb/configs/initscript.sh
+++ roles/tasks/mariadb/configs/initscript.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 mysql_install_db
-rc-service mysql start
-mysql < /usr/local/bin/mysqlsecureinstallation.sql
-mysql < /usr/local/bin/create_users.sql
-mysql < /usr/local/bin/config_wp.sql  
-rc-service mysql restart
 
+echo "CREATE DATABASE IF NOT EXISTS wordpress; FLUSH PRIVILEGES;" | mysql -u root
+echo "CREATE DATABASE IF NOT EXISTS phpmyadmin; FLUSH PRIVILEGES;" | mysql -u root
+echo "CREATE USER IF NOT EXISTS '$DB_USER'@'%' IDENTIFIED by '$DB_PASS'; FLUSH PRIVILEGES;" | mysql -u root
+echo "CREATE USER IF NOT EXISTS '$PMA_USER'@'%' IDENTIFIED by '$PMA_PASS'; FLUSH PRIVILEGES;" | mysql -u root
+echo "GRANT ALL PRIVILEGES ON phpmyadmin.* TO '$PMA_USER'@'%' IDENTIFIED BY '$PMA_PASS'; FLUSH PRIVILEGES;" | mysql -u root
+echo "GRANT ALL PRIVILEGES ON wordpress.* TO '$DB_USER'@'%' IDENTIFIED BY '$DB_PASS'; FLUSH PRIVILEGES;" | mysql -u root
+
+# mysql < /usr/local/bin/config_wp.sql  
 # mysqld --bind-address=0.0.0.0
+mysqld_safe
