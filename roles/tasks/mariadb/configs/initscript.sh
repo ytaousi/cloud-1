27af67bad252662235ab0b4ca3463e5ba26608d5
ytaousi <yasssir.taous@gmail.com>
2023-09-14 14:10:20 +0100

    ... db crash every 1.01m

--- roles/tasks/mariadb/configs/initscript.sh
+++ roles/tasks/mariadb/configs/initscript.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
-# mysql_install_db
-# rc-service mysql start
-# mysql < mysql_secure_installation.sql 
-# mysql < create_users.sql -u root -p$DB_ROOT_PASS
-# mysql < config_wp.sql -u root -p$DB_ROOT_PASS
-# rc-service mysql restart
+mysql_install_db
+rc-service mysql start
+mysql < /usr/local/bin/mysqlsecureinstallation.sql
+mysql < /usr/local/bin/create_users.sql
+mysql < /usr/local/bin/config_wp.sql  
+rc-service mysql restart
 
-sleep 3000
 # mysqld --bind-address=0.0.0.0
