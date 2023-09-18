7467b8dab78fba029363422366887231bd4c6a6c
ytaousi <yassir.taous@gmail.com>
2023-09-18 19:34:27 +0100

    wa baraka men lebka a da7aya

--- srcs/roles/wordpress_phpmyadmin/configs/wp-config.php
+++ srcs/roles/wordpress_phpmyadmin/configs/wp-config.php
@@ -20,13 +20,13 @@
 
 // ** Database settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define( 'DB_NAME', '' );
+define( 'DB_NAME', 'wordpress' );
 
 /** Database username */
-define( 'DB_USER', '' );
+define( 'DB_USER', 'wp-user' );
 
 /** Database password */
-define( 'DB_PASSWORD', '' );
+define( 'DB_PASSWORD', 'wp-password' );
 
 /** Database hostname */
 define( 'DB_HOST', 'mariadb' );
